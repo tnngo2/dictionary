0a314dc4dbe65648e9a482dd9485f62fcc5a60c7
Tuan Ngo <tnngo2@gmail.com>
2014-11-28 00:11:36 +0800

    Initialize staffs

---
+++ Dictionary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dictionary
+//
+//  Created by Tuan Ngo on 11/27/14.
+//  Copyright (c) 2014 Tuan Ngo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
